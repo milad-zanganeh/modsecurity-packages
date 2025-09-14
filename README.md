829a45dde585f139d6669c16dee69349bc92065d
Milad Zangeneh <miluxhd@gmail.com>
2025-09-14 22:32:26 +0200

    migrate files from gitlab to github

---
+++ README.md
@@ -0,0 +1,12 @@
+# Nginx ModSecurity Packages
+
+This repository contains the build configuration for creating Nginx packages with ModSecurity integration for multiple Ubuntu and Debian distributions.
+
+## Overview
+
+This project builds Nginx packages with ModSecurity 3.0.8 and the Core Rule Set (CRS) for the following distributions:
+- Ubuntu 20.04 (Focal)
+- Ubuntu 22.04 (Jammy) 
+- Ubuntu 24.04 (Noble)
+- Debian 11 (Bullseye)
+- Debian 12 (Bookworm)
---
+++ build.sh
@@ -0,0 +1,65 @@
+export DEBIAN_FRONTEND=noninteractive
+
+set -e 
+cp  apt-proxy /etc/apt/apt.conf.d/00proxy
+apt update && apt install -y \
+    build-essential \
+    dpkg-dev \
+    devscripts \
+    debhelper \
+    fakeroot \
+    libpcre3-dev \
+    zlib1g-dev \
+    libssl-dev \
+    git \
+    software-properties-common \
+    curl \
+    gnupg2 \
+    ca-certificates \
+    lsb-release \
+    pkg-config \
+    libyajl-dev \
+    autoconf \
+    automake \
+    libtool \
+    libcurl4-openssl-dev \
+    libxml2-dev \
+    libgeoip-dev \
+    libpcre2-dev \
+    quilt \
+    vim \
+    wget
+
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor -o /usr/share/keyrings/nginx-archive-keyring.gpg \
+    && echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/$(lsb_release -is | tr '[:upper:]' '[:lower:]') $(lsb_release -cs) nginx" \
+    | tee /etc/apt/sources.list.d/nginx.list \
+    && echo "deb-src [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/$(lsb_release -is | tr '[:upper:]' '[:lower:]') $(lsb_release -cs) nginx" \
+    >> /etc/apt/sources.list.d/nginx.list
+
+/bin/bash -c 'echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n"  | tee /etc/apt/preferences.d/99nginx'
+
+
+mkdir -p /nginx && apt update && (cd /nginx && apt source nginx && mv  $(ls -d /nginx/nginx-*) /nginx/nginx-latest)
+cp $CI_PROJECT_DIR/rules /nginx/nginx-latest/debian/rules
+
+git clone --depth 1 -b v3.0.8 https://github.com/SpiderLabs/ModSecurity.git \
+    && (cd ModSecurity \
+    && git submodule update --init \
+    && ./build.sh \
+    && ./configure --with-yajl \
+    && make -j$(nproc) \
+    && make install \
+    && mkdir -p /usr/local/modsecurity/etc \
+    && cp modsecurity.conf-recommended /usr/local/modsecurity/etc/modsecurity.conf \
+    && wget -O /usr/local/modsecurity/etc/unicode.mapping https://raw.githubusercontent.com/SpiderLabs/ModSecurity/v3/master/unicode.mapping)
+
+git clone --depth 1 https://github.com/SpiderLabs/ModSecurity-nginx.git && mv ModSecurity-nginx /usr/src/modsecurity
+
+git clone --depth 1 https://github.com/coreruleset/coreruleset.git /etc/nginx/modsec
+
+cp $CI_PROJECT_DIR/modsec/main.conf /etc/nginx/modsec/main.conf
+
+(cd /nginx/nginx-latest && dpkg-buildpackage -b -us -uc)
+md5sum /nginx/*.deb
+
+ls /nginx/nginx*.deb
