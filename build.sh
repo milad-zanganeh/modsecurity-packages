1fbba6ced41c1a580d893063dad2aeff3ae518b6
Milad Zangeneh <miluxhd@gmail.com>
2025-09-24 19:25:52 +0200

    force nginx version in apt source

--- build.sh
+++ build.sh
@@ -1,33 +1,18 @@
-export DEBIAN_FRONTEND=noninteractive
-CI_PROJECT_DIR=$PWD
+#!/bin/bash
 set -e 
 
+export DEBIAN_FRONTEND=noninteractive
+CI_PROJECT_DIR=$PWD
+NGINX_VERSION_REQ=$(echo "$TAG_NAME" | sed -E 's/^nginx-//; s/^[vV]//')
+
+
 apt update && apt install -y \
-    build-essential \
-    dpkg-dev \
-    devscripts \
-    debhelper \
-    fakeroot \
-    libpcre3-dev \
-    zlib1g-dev \
-    libssl-dev \
-    git \
     software-properties-common \
     curl \
     gnupg2 \
     ca-certificates \
     lsb-release \
     pkg-config \
-    libyajl-dev \
-    autoconf \
-    automake \
-    libtool \
-    libcurl4-openssl-dev \
-    libxml2-dev \
-    libgeoip-dev \
-    libpcre2-dev \
-    quilt \
-    vim \
     wget
 
 DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
@@ -38,12 +23,40 @@ curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor -o /usr/share/keyr
     && echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/$DISTRO $RELEASE nginx" \
     | tee /etc/apt/sources.list.d/nginx.list \
     && echo "deb-src [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/$DISTRO $RELEASE nginx" \
-    >> /etc/apt/sources.list.d/nginx.list
+    >> /etc/apt/sources.list.d/nginx.list 
 
-/bin/bash -c 'echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n"  | tee /etc/apt/preferences.d/99nginx'
+/bin/bash -c 'echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n"  | tee /etc/apt/preferences.d/99nginx' && apt update
+
+SRC_VER=$(apt-cache showsrc nginx | awk '/^Version:/ {print $2}' | grep -E "^${NGINX_VERSION_REQ}([~+:-]|$)" | head -n1 || true)
+echo "SRC_VER: $SRC_VER"
+
+if [ -z "$SRC_VER" ]; then
+    echo "ERROR: Requested NGINX version '$NGINX_VERSION_REQ' not found for $DISTRO $RELEASE in nginx.org repository."
+    exit 1
+fi
+
+apt apt install -y \
+    build-essential \
+    dpkg-dev \
+    devscripts \
+    debhelper \
+    fakeroot \
+    libpcre3-dev \
+    zlib1g-dev \
+    libssl-dev \
+    git \
+    libyajl-dev \
+    autoconf \
+    automake \
+    libtool \
+    libcurl4-openssl-dev \
+    libxml2-dev \
+    libgeoip-dev \
+    libpcre2-dev \
+    quilt
 
 
-mkdir -p /nginx && apt update && (cd /nginx && apt source nginx && mv  $(ls -d /nginx/nginx-*) /nginx/nginx-latest)
+mkdir -p /nginx && apt update && (cd /nginx && apt source nginx=$NGINX_VERSION_REQ && mv  $(ls -d /nginx/nginx-*) /nginx/nginx-latest)
 cp $CI_PROJECT_DIR/rules /nginx/nginx-latest/debian/rules
 
 git clone --depth 1 -b v3.0.8 https://github.com/SpiderLabs/ModSecurity.git \
